94a6c5a59e45572d0f9883e09c2d8735d1ac58ef
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-06-13 01:38:46 -0500

    Создает функции

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+const lengthString = (str, maxLength) => str.length <= maxLength;
+
+const isPalindrome = function (string) {
+  const newString = string.replaceAll(' ', '').toLowerCase();
+  let result = '';
+  for (let i = newString.length - 1; i >= 0; i--) {
+    result += newString[i];
+  }
+  return result === newString;
+};
+
+const getPositiveNumber = function (data) {
+  let str = data;
+  if (typeof str !== 'string') {
+    str = str.toString();
+  }
+
+  let result = '';
+  for (let i = 0; i <= str.length; i++) {
+    const num = parseInt(str[i], 10);
+    if (!Number.isNaN(num)) {
+      result += num;
+    }
+  }
+  return result;
+};
