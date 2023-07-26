b64133da60a84a07ba8528d0c52749a377c3121a
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-07-26 11:57:39 -0500

    Перламутровые пуговицы

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,20 @@
 import { renderImg } from './miniature-drawer.js';
 import { openFullSize } from './full-screen-image.js';
+import { generateRandomPosts } from './filters.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Ваш код здесь
+  const buttonRandom = document.querySelector('#filter-random');
+  const pictures = document.querySelectorAll('.picture');
   const getData = () => {
     fetch('https://29.javascript.pages.academy/kekstagram/data')
       .then((response) => response.json())
       .then((data) => {
         renderImg(data);
+        buttonRandom.addEventListener('click', () => {
+          pictures.textContent = '';
+          renderImg(generateRandomPosts(data));
+          console.log('dddd')
+        });
       })
       .then((dat) => {
         openFullSize(dat);
@@ -16,7 +23,8 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Error fetching data:', error);
       });
   };
-
   getData();
+
 });
 // export { getData };
+
